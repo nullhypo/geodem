7afc9461f942e9889c4b2ac3844481212e3f22ef
nullhypo <ben.st@hotmail.com>
2019-11-30 22:48:23 +0000

    Initial commit

--- venv/features.py
+++ venv/features.py
@@ -1,37 +1,50 @@
 from setup import *
 
+mdir = path + '/data/metadata/feature_categorisation.csv'  # feature categorisation metadata file
+idir = path + '/data/input_tests'  # features input file path
+odir = path + '/data/features'  # features output path
 
+md = pd.read_csv(mdir)  # read in feature categorisation metadata file
+fl = md['feature'].unique()
 
-mdir = path + '/data/metadata/feature_categorisation.csv'
-idir = path + '/data/inputs'
-odir = path + '/data/features'
+files = os.listdir(odir)
+for f in files:
+    os.remove(odir + '/' + f)
 
-md = pd.read_csv(mdir)
+def features(feature, input_feature):
 
+    print(input_feature)
+    df = pd.read_csv(idir + '/' + str(input_feature) + '_cleaned.csv')  # read in feature input file
+    df = df.head(1000)  # for dev
+    mdi = md[md['feature'] == feature]
+    df = pd.merge(df, mdi, how='inner', left_on=['level'], right_on=['level'])  # join feature input file to metadata
 
-file = 'oa11_Age_Adult_cleaned.csv'
-#for file in os.listdir(dir):
-print(file)
-feature = file[:-12]
+    # order oas by level and create cumulative sums of metric against each level
+    df = df.sort_values(['oa11', 'order'], ascending=[True, True])
+    df['cuml'] = df.groupby(['oa11'])['metric'].apply(lambda x: x.cumsum())
 
-df = pd.read_csv(idir + '/' + str(file))
+    # determine maximum cumulative value, number of levels and splitting level
+    mx = (df.groupby(['oa11']).agg(max=('cuml', 'max'))).reset_index()
+    ng = df['level'].nunique()
+    ln = int(mdi[mdi['split'] == 1]['order'])
 
-df = df.head(1000)
+    # calculate cumulative proportions of maximum for each level
+    df = pd.merge(df, mx, how='inner', left_on=['oa11'], right_on=['oa11'])
+    df['cuml_prop'] = (df['cuml'] + (df['order'] / ng)) / (df['max'] + 1)
 
-df = pd.merge(df, md, how='inner', left_on=['level'], right_on=['level'])
+    # calculate log odds for the splitting level and write as csv to feature output directory
+    df = df[df['order'] == ln]
+    df[feature] = np.log(df['cuml_prop']) - (np.log(1 - (df['cuml_prop'])))
+    df = df[['oa11', feature]]
+    np.testing.assert_array_equal(df['oa11'].nunique(), df['oa11'].count())
+    df.to_csv(odir + '/' + feature + '.csv', index=False)
 
-df = df.sort_values(['oa11', 'order'], ascending=[True, True])
-df['cuml'] = df.groupby(['oa11'])['metric'].apply(lambda x: x.cumsum())
-mx = (df.groupby(['oa11']).agg(max=('cuml', 'max'))).reset_index()
-ng = df['level'].nunique()
-ln = int(md[md['split'] == 1]['order'])
-df = pd.merge(df, mx, how='inner', left_on=['oa11'], right_on=['oa11'])
-df['cuml_prop'] = (df['cuml'] + (df['order'] / ng)) / (df['max'] + 1)
-df = df[df['order'] == ln]
-df[feature] = np.log(df['cuml_prop']) - (np.log(1 - (df['cuml_prop'])))
-df = df[['oa11', feature]]
+for f in fl:
+    mdi = md[md['feature'] == f]
+    input_feature = mdi['input_feature'].unique()[0]
+    feature = mdi['feature'].unique()[0]
+    features(feature, input_feature)
 
-df.to_csv(odir + '/' + feature + '.csv', index=False)
 
 
 
