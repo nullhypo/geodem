68f106e7697a0fe56d5a1c569e3501cee487b1f8
nullhypo <ben.st@hotmail.com>
2019-12-04 23:03:06 +0000

    Initial commit

--- venv/features.py
+++ venv/features.py
@@ -1,49 +1,75 @@
 from setup import *
 
-mdir = path + '/data/metadata/feature_categorisation.csv'  # feature categorisation metadata file
-idir = path + '/data/input_tests'  # features input file path
-odir = path + '/data/features'  # features output path
 
-md = pd.read_csv(mdir)  # read in feature categorisation metadata file
-fl = md['feature'].unique()
+class Features:
 
-files = os.listdir(odir)
-for f in files:
-    os.remove(odir + '/' + f)
+    # class attributes
+    oadir = path + '/data/spines/oa11'
 
-def features(feature, input_feature):
+    def __init__(self, project_id):
 
-    print(input_feature)
-    df = pd.read_csv(idir + '/' + str(input_feature) + '_cleaned.csv')  # read in feature input file
-    df = df.head(1000)  # for dev
-    mdi = md[md['feature'] == feature]
-    df = pd.merge(df, mdi, how='inner', left_on=['level'], right_on=['level'])  # join feature input file to metadata
+        self.project_id = project_id
 
-    # order oas by level and create cumulative sums of metric against each level
-    df = df.sort_values(['oa11', 'order'], ascending=[True, True])
-    df['cuml'] = df.groupby(['oa11'])['metric'].apply(lambda x: x.cumsum())
+        mdir = path + '/data/metadata/' + self.project_id  # feature categorisation metadata file
+        idir = path + '/data/inputs/' + self.project_id  # features input file path
+        odir = path + '/data/features/' + self.project_id  # features output path
+        lohdir = path + '/data/log_odds_histograms/' + self.project_id
 
-    # determine maximum cumulative value, number of levels and splitting level
-    mx = (df.groupby(['oa11']).agg(max=('cuml', 'max'))).reset_index()
-    ng = df['level'].nunique()
-    ln = int(mdi[mdi['split'] == 1]['order'])
+        pp = PdfPages(lohdir + '/log_odds_histograms.pdf')  # open model_outputs pdf for writing
+        md = pd.read_csv(mdir + '/feature_categorisation.csv')  # read in feature categorisation metadata file
+        fl = md['feature'].unique()
 
-    # calculate cumulative proportions of maximum for each level
-    df = pd.merge(df, mx, how='inner', left_on=['oa11'], right_on=['oa11'])
-    df['cuml_prop'] = (df['cuml'] + (df['order'] / ng)) / (df['max'] + 1)
+        oa = pd.read_csv(self.oadir + '/oa11_spine.csv')
 
-    # calculate log odds for the splitting level and write as csv to feature output directory
-    df = df[df['order'] == ln]
-    df[feature] = np.log(df['cuml_prop']) - (np.log(1 - (df['cuml_prop'])))
-    df = df[['oa11', feature]]
-    np.testing.assert_array_equal(df['oa11'].nunique(), df['oa11'].count())
-    df.to_csv(odir + '/' + feature + '.csv', index=False)
+        files = os.listdir(odir)
+        for f in files:
+            os.remove(odir + '/' + f)
 
-for f in fl:
-    mdi = md[md['feature'] == f]
-    input_feature = mdi['input_feature'].unique()[0]
-    feature = mdi['feature'].unique()[0]
-    features(feature, input_feature)
+        def features(feature, input_feature):
+
+            print(input_feature)
+            df = pd.read_csv(idir + '/' + str(input_feature) + '_cleaned.csv')  # read in feature input file
+            df = pd.merge(df, oa, how='inner', left_on=['oa11'], right_on=['oa11'])  # join feature input file to oa spine
+            #df = df.head(1000)  # for dev
+            mdi = md[md['feature'] == feature]
+            df = pd.merge(df, mdi, how='inner', left_on=['level'], right_on=['level'])  # join feature input file to metadata
+
+            # order oas by level and create cumulative sums of metric against each level
+            df = df.sort_values(['oa11', 'order'], ascending=[True, True])
+            df['cuml'] = df.groupby(['oa11'])['metric'].apply(lambda x: x.cumsum())
+
+            # determine maximum cumulative value, number of levels and splitting level
+            mx = (df.groupby(['oa11']).agg(max=('cuml', 'max'))).reset_index()
+            ng = df['level'].nunique()
+            ln = int(mdi[mdi['split'] == 1]['order'])
+
+            # calculate cumulative proportions of maximum for each level
+            df = pd.merge(df, mx, how='inner', left_on=['oa11'], right_on=['oa11'])
+            df['cuml_prop'] = (df['cuml'] + (df['order'] / ng)) / (df['max'] + 1)
+
+            # calculate log odds for the splitting level and write as csv to feature output directory
+            df = df[df['order'] == ln]
+            df[feature] = np.log(df['cuml_prop']) - (np.log(1 - (df['cuml_prop'])))
+            df = df[['oa11', feature]]
+            np.testing.assert_array_equal(df['oa11'].nunique(), df['oa11'].count())
+            df.to_csv(odir + '/' + feature + '.csv', index=False)
+
+            # histogram of log odds
+            fig = plt.figure()
+            plt.hist(df[feature], bins=100)
+            plt.title(feature + ' histogram', fontsize=16)
+            plt.xlabel(feature)
+            plt.ylabel('frequency')
+            pp.savefig()
+
+        # Loop through features in metadata file and run features function for each
+        for f in fl:
+            mdi = md[md['feature'] == f]
+            input_feature = mdi['input_feature'].unique()[0]
+            feature = mdi['feature'].unique()[0]
+            features(feature, input_feature)
+
+        pp.close()
 
 
 
--- venv/setup.py
+++ venv/setup.py
@@ -6,41 +6,26 @@
 ################################################################
 
 # import libraries
-global project_id,path,pd,np,mt,dt,mp,plt,pdf,mse,r2s,sqrt,DataFrame,monthrange,isleap,sqldf,PdfPages,pp,randint,pysqldf,create_grid,cl,linear_model,sm, TheilSenRegressor, RANSACRegressor, ss, confusion_matrix, gc, pyodbc, multiprocessing, time, Pool, f1_score, itertools, os, gc, logging
-import numpy as np
+global path, pd, np, sc, mp, sns, plt, sk, mt, sqldf, linear_model, pyodbc, datetime, pdf, PdfPages, spatial, md, PCA, gc, figure, pp, cl, Axes3D, ss, os
 import pandas as pd
-import math as mt
-import datetime as dt
+import numpy as np
+import scipy as sc
 import matplotlib as mp
+import seaborn as sns
 import matplotlib.pyplot as plt
+import sklearn as sk
+import math as mt
 import matplotlib.backends.backend_pdf as pdf
-from sklearn.metrics import mean_squared_error as mse
-from sklearn.metrics import r2_score as r2s
-from sklearn import linear_model as linear_model
-from math import sqrt as sqrt
-from pandas import DataFrame as DataFrame
-from calendar import monthrange as monthrange
-from calendar import isleap as isleap
 from matplotlib.backends.backend_pdf import PdfPages as PdfPages
-from random import randint as randint
+from scipy import spatial as spatial
+from sklearn import manifold as md
+from sklearn.decomposition import PCA as PCA
+from pylab import figure
+from sklearn import preprocessing as pp
 from sklearn import cluster as cl
-from sklearn import linear_model as linear_model
-import statsmodels.api as sm
-from sklearn.linear_model import TheilSenRegressor as TheilSenRegressor
-from sklearn.linear_model import RANSACRegressor as RANSACRegressor
+from mpl_toolkits.mplot3d import Axes3D
 from scipy import stats as ss
-from sklearn.metrics import confusion_matrix as confusion_matrix
-import gc as gc
-import pyodbc as pyodbc
-import multiprocessing as multiprocessing
-from time import time as time
-from multiprocessing import Pool as Pool
-from sklearn.metrics import f1_score as f1_score
-import itertools as itertools
 import os as os
-import gc as gc
-import logging as logging
-
 
 # path for project
 path = 'C:/work/yellow_zebra/geodem'
